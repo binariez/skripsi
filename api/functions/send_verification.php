3accb56fd1759582d8b52e9247ead94c0c8bb6df
Azhar SS <azhacra@gmail.com>
2025-09-20 22:32:32 +0700

    route v2

--- api/functions/send_verification.php
+++ api/functions/send_verification.php
@@ -3,7 +3,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 function sendVerificationEmail($email, $token)
 {
