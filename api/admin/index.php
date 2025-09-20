3d286c1d6ce960ac9cb846b0e3ec9c793143e26b
Azhar SS <azhacra@gmail.com>
2025-09-21 01:16:05 +0700

    fix typo

--- api/admin/index.php
+++ api/admin/index.php
@@ -4,7 +4,7 @@ ob_start();
 
 require_once __DIR__ . '/../functions/Sessions.php';
 require_once __DIR__ . '/func/CRUD.php';
-require_once 'functions/SessionHandlerInterface.php';
+require_once __DIR__ . '/../functions/SessionHandlerInterface.php';
 session_start();
 
 // Cek apakah user sudah login
