c661c6da0d800c3352dc871ccdd7c6186a6fdffd
Azhar SS <azhacra@gmail.com>
2025-09-20 23:45:09 +0700

    fixing session_destroy

--- api/logout.php
+++ api/logout.php
@@ -2,8 +2,7 @@
 require_once 'functions/SessionHandlerInterface.php';
 session_start();
 if (session_destroy()) {
-    // echo "logout sukses";
-    header("Location: index.php");
+    echo "logout sukses";
 } else {
     echo "logout gagal";
 }
